302c6d9edc06fbdbb3c655c03e2c8503cfaacdeb
renanrecio <renanrecio@gmail.com>
2018-02-21 08:28:04 -0300

    ajustes

--- www/config_loja.js
+++ www/config_loja.js
@@ -4,5 +4,5 @@ var versao_app = 'v1.0_15_02_2018';//'v1.0_05_02_2018';
 
 var nome_da_loja_do_app = "PetShop";
 
-var url_servidor = "http://54.207.38.125/AcheLoja2.0/";
+//var url_servidor = "http://54.207.38.125/AcheLoja2.0/";
 //var url_servidor = "https://acheloja.com/";
